079e2f14f3b2d05f00eb68df403ed1e6098dd1dd
Pon <Nguyenhuuphong2k3@gmail.com>
2025-03-28 05:31:08 +0700

    Update

--- src/main/java/com/nhom17/fashionshop/repository/OrderDetailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nhom17.fashionshop.repository;
-
-import com.nhom17.fashionshop.model.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
-}
--- src/main/java/com/nhom17/fashionshop/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nhom17.fashionshop.repository;
-
-import com.nhom17.fashionshop.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
